8cbd39059893dd122b2f06d9c9fb34ba60f6e2f0
Charlotte Cleary <CCleary2@uclan.ac.uk>
2024-10-05 17:37:37 +0100

    Added collision detection between two squares.

--- SATTesting.cpp
+++ SATTesting.cpp
@@ -30,23 +30,32 @@ struct Square
 	void UpdateAxesArray();
 };
 
+struct CollisionData
+{
+	Vector2 mAxis;
+	float mPenetration;
+	Vector2 mPointOnPlane;
+};
+
 struct CoolCube
 {
 	Model* CubeModel;
 	Model* Vertices[];
 };
 
-void TwoSquaresSAT(Square& Sq1, Square& Sq2);
+bool TwoSquaresSAT(Square& Sq1, Square& Sq2);
+bool CheckCollisionAxisSquares(const Vector2& Axis, const Square& Sq1, const Square& Sq2);
+void GetMinMaxVertexOnAxisSquare(const Vector2& Axis, const Square& Sq, float& Min, float& Max);
 
 int main()
 {
 	// Create a 3D engine (using TL11 engine here) and open a window for it
-	TLEngine* myEngine = New3DEngine( TL11 );
+	TLEngine* myEngine = New3DEngine(TL11);
 	myEngine->StartWindowed();
 
 	// Add default folder for meshes and other media
-	//myEngine->AddMediaFolder( "C:\\Users\\Public\\Documents\\TL-Engine11\\Media" );
-	myEngine->AddMediaFolder(".\\Media");
+	myEngine->AddMediaFolder("C:\\Users\\Public\\Documents\\TL-Engine11\\Media");
+	//myEngine->AddMediaFolder(".\\Media");
 
 	/**** Set up your scene here ****/
 	Mesh* CubeMesh = myEngine->LoadMesh("Cube.fbx");
@@ -106,8 +115,20 @@ int main()
 			Test.CentreDummy->MoveLocalX(DeltaTime * MoveSpeed);
 		}
 
+		// Check collision
+		if (TwoSquaresSAT(Test, Test2))
+		{
+			Test.CentreDummy->SetSkin("RedBall.jpg");
+			Test2.CentreDummy->SetSkin("RedBall.jpg");
+		}
+		else
+		{
+			Test.CentreDummy->SetSkin("Grass1.jpg");
+			Test2.CentreDummy->SetSkin("Grass1.jpg");
+		}
+
 		// Stop if the Escape key is pressed
-		if (myEngine->KeyHit( Key_Escape ))
+		if (myEngine->KeyHit(Key_Escape))
 		{
 			myEngine->Stop();
 		}
@@ -136,13 +157,13 @@ float Vector2::Length() const
 // Subtracts the passed in vector from the vector and returns the result.
 Vector2 Vector2::Subtract(const Vector2& OtherVec) const
 {
-	return Vector2((x-OtherVec.x), (y-OtherVec.y));
+	return Vector2((x - OtherVec.x), (y - OtherVec.y));
 }
 
 // Returns the dot product between the two vectors.
 float Vector2::DotProduct(const Vector2& OtherVec) const
 {
-	return (x*OtherVec.x + y*OtherVec.y);
+	return (x * OtherVec.x + y * OtherVec.y);
 }
 
 // Sets up the square
@@ -204,36 +225,86 @@ void Square::UpdateAxesArray()
 	}
 }
 
-void TwoSquaresSAT(Square& Sq1, Square& Sq2)
+bool TwoSquaresSAT(Square& Sq1, Square& Sq2)
 {
 	// Udpate vertices positions of both squares
 	Sq1.UpdateVerticesPosition();
-	Sq2.UpdateVerticesPosition();	
+	Sq2.UpdateVerticesPosition();
 
 	// Update axes of first square
 	Sq1.UpdateAxesArray();
 
-	// Loop through axes array of first sqaure
+	// Check each axis for collision. If any return false then there is no collision.
 	for (int i = 0; i < SquareNumAxesToCheck; i++)
 	{
-		// Project each corner onto the axis and keep track of min and max.
-		Vector2 projMinMaxArr[SquareNumAxesToCheck];
-		projMinMaxArr[i].x = Sq1.VerticesPositionArray[0].DotProduct(Sq1.AxesArray[i]);
-		projMinMaxArr[i].y = projMinMaxArr[i].x;
-
-		for (int j = 1; j < SquareNumCorners; j++)
+		if (!CheckCollisionAxisSquares(Sq1.AxesArray[i], Sq1, Sq2))
 		{
-			/*projection = Sq1.VerticesPositionArray[j].DotProduct(Sq1.AxesArray[i]);
+			return false;
+		}
+	}
 
-			if (projection < p1Min)
-			{
-				p1Min = projection;
-			}
+	// Update axes of second square
+	Sq2.UpdateAxesArray();
 
-			if (projection > p1Max)
-			{
-				p1Max = projection;
-			}*/
+	// Check each axis for collision.
+	for (int i = 0; i < SquareNumAxesToCheck; i++)
+	{
+		if (!CheckCollisionAxisSquares(Sq2.AxesArray[i], Sq1, Sq2))
+		{
+			return false;
+		}
+	}
+
+	// Must be colliding if reach this point!
+	return true;
+}
+
+// Using this link for the outline of the implementation. 
+// https://research.ncl.ac.uk/game/mastersdegree/gametechnologies/previousinformation/physics4collisiondetection/2017%20Tutorial%204%20-%20Collision%20Detection.pdf
+bool CheckCollisionAxisSquares(const Vector2& Axis, const Square& Sq1, const Square& Sq2)
+{
+	// point A = min on shape 1, point B = max on shape 1.
+	// point C = min on shape 2, point D = max on shape 2.
+
+	// get A,B,C,D
+	float A, B, C, D;
+	GetMinMaxVertexOnAxisSquare(Axis, Sq1, A, B);
+	GetMinMaxVertexOnAxisSquare(Axis, Sq2, C, D);
+
+	// Overlap test - first way (A < C AND B > C)
+	if (A <= C && B >= C)
+	{
+		return true;
+	}
+
+	// Overlap test - second way (C < A AND D > A)
+	if (C <= A && D >= A)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+void GetMinMaxVertexOnAxisSquare(const Vector2& Axis, const Square& Sq, float& Min, float& Max)
+{
+	// Assume initial min/max
+	Min = Sq.VerticesPositionArray[0].DotProduct(Axis);
+	Max = Min;
+
+	// Loop through remaining vertices to find min/max
+	for (int i = 1; i < SquareNumCorners; i++)
+	{
+		float Projection = Sq.VerticesPositionArray[i].DotProduct(Axis);
+
+		if (Projection < Min)
+		{
+			Min = Projection;
+		}
+
+		if (Projection > Max)
+		{
+			Max = Projection;
 		}
 	}
 }
